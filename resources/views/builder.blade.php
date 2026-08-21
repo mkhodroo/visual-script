4112064280525442ae507e945e3c1dadd5b33239
behinIndustrialGroup <166484385+behinIndustrialGroup@users.noreply.github.com>
2026-08-21 20:57:43 +0330

    Update builder.blade.php

--- resources/views/builder.blade.php
+++ resources/views/builder.blade.php
@@ -97,6 +97,6 @@
     window.VS_PREVIEW_URL = "{{ route('visual-script.preview') }}";
     window.VS_CSRF = "{{ csrf_token() }}";
 </script>
-<script src="{{ url('public/vendor/visual-script/js/nodes/save-node.js') }}"></script>
-<script src="{{ url('public/vendor/visual-script/js/builder.js') }}"></script>
+<script src="{{ url('public/vendor/visual-script/js/nodes/save-node.js?v=1.0') }}"></script>
+<script src="{{ url('public/vendor/visual-script/js/builder.js?v=1.0') }}"></script>
 @endpush
